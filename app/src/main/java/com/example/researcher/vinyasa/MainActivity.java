0b78a570fb12601888b2ca8f7a1f0a88cc39ab29
Vidhya Balasubramanian <indooris.amrita@gmail.com>
2019-02-23 21:51:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/researcher/vinyasa/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.researcher.vinyasa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_main);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(MainActivity.this,Map.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
---
+++ app/src/main/java/com/example/researcher/vinyasa/Map.java
@@ -0,0 +1,13 @@
+package com.example.researcher.vinyasa;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Map extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.next_act);
+    }
+}
